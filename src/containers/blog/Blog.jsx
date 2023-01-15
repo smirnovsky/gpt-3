4c4d38c1e0945ee867e4cbe73c25edc0e11226b2
smirnovsky <happy280190@gmail.com>
2023-01-15 19:57:20 +0300

    feat: add markup for footer

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
         <div className='gpt3__blog-container_groupA'>
           <Article imgUrl={blog01} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
         </div>
-        <div className='gpt3__blog-container_groupA'>
+        <div className='gpt3__blog-container_groupB'>
           <Article imgUrl={blog02} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
           <Article imgUrl={blog03} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
           <Article imgUrl={blog04} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
